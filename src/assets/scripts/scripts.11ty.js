fb30d3ae8ef7dfdc6a82d4cad19b68f31a32778d
Juan Villela <juan@villela.co>
2021-06-01 19:23:49 -0400

    feat: Update scripts formatter to utilize Bable and Terser.

--- src/assets/scripts/scripts.11ty.js
+++ src/assets/scripts/scripts.11ty.js
@@ -1,90 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-const webpack = require('webpack');
-const MemoryFileSystem = require('memory-fs');
+/* eslint-disable class-methods-use-this */
+const { join } = require('path');
+const { readFileSync } = require('fs');
 
-const isProd = process.env.ELEVENTY_ENV === 'production';
-const mfs = new MemoryFileSystem();
+const { minify } = require('terser');
+const { transformAsync } = require('@babel/core');
 
-// main entry point name
+// file paths
 const ENTRY_FILE_NAME = 'main.js';
+const entryPath = join(__dirname, `/${ENTRY_FILE_NAME}`);
+const jsData = readFileSync(entryPath, 'utf8', data => data);
 
 module.exports = class {
-  // Configure Webpack in Here
-  async data() {
-    const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`);
-    const outputPath = path.resolve(__dirname, '../../memory-fs/js/');
-
-    // Transform .js files, run through Babel
-    const rules = [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ];
-
-    // pass environment down to scripts
-    const envPlugin = new webpack.EnvironmentPlugin({
-      ELEVENTY_ENV: process.env.ELEVENTY_ENV,
-    });
-
-    // Main Config
-    const webpackConfig = {
-      mode: isProd ? 'production' : 'development',
-      entry: entryPath,
-      output: { path: outputPath },
-      module: { rules },
-      plugins: [envPlugin],
-    };
-
+  // template "frontmatter"
+  data() {
     return {
-      permalink: `/assets/scripts/${ENTRY_FILE_NAME}`,
       eleventyExcludeFromCollections: true,
-      webpackConfig,
+      layout: false,
+      permalink: `/assets/scripts/${ENTRY_FILE_NAME}`,
     };
   }
 
-  // Compile JS with Webpack, write the result to Memory Filesystem.
-  // this brilliant idea is taken from Mike Riethmuller / Supermaya
-  // @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-webpack.js
-  compile(webpackConfig) {
-    const compiler = webpack(webpackConfig);
-    compiler.outputFileSystem = mfs;
-    compiler.inputFileSystem = fs;
-    compiler.resolvers.normal.fileSystem = mfs;
-
-    return new Promise((resolve, reject) => {
-      compiler.run((err, stats) => {
-        if (err || stats.hasErrors()) {
-          const errors =
-            err || (stats.compilation ? stats.compilation.errors : null);
-
-          reject(errors);
-          return;
-        }
-
-        const { assets } = stats.compilation;
-        const file = assets[ENTRY_FILE_NAME].source();
-
-        resolve(file);
-      });
-    });
-  }
-
-  // render the JS file
-  async render({ webpackConfig }) {
+  async render() {
     try {
-      const result = await this.compile(webpackConfig);
+      // transpile with babel; uses local config file and browserslist in package.json
+      const compiled = await transformAsync(jsData).then(result => result.code);
+      const minified = await minify(compiled);
 
-      return result;
-    } catch (err) {
-      console.error('[Scripts Render]:', err);
+      return minified.code;
+    } catch (error) {
+      console.error('[Scripts Render]:', error);
 
       return null;
     }
