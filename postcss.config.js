364a3891bb459b082be88bdfab4da3724b6b3851
Juan Villela <juan@villela.co>
2021-06-01 16:40:01 -0400

    feat: Add PostCSS config.

---
+++ postcss.config.js
@@ -0,0 +1,33 @@
+// Learn more about PostCSS:
+// https://github.com/postcss/postcss
+
+const cssnano = require('cssnano');
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = () => ({
+  plugins: [
+    postcssPresetEnv({
+      stage: 3,
+      features: {
+        'custom-properties': {
+          preserve: true,
+          fallback: true,
+        },
+      },
+      autoprefixer: {
+        flexbox: true,
+        grid: false,
+      },
+    }),
+    cssnano({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ],
+    }),
+  ],
+});
