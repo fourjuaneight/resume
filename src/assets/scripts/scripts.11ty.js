5d264e35e42934b8c9193b9bbebf905cac8eec66
Juan Villela <juan@villela.co>
2021-06-03 10:50:21 -0400

    fix: Move bundle script reading after bundle.

--- src/assets/scripts/scripts.11ty.js
+++ src/assets/scripts/scripts.11ty.js
@@ -1,17 +1,18 @@
 /* eslint-disable class-methods-use-this */
 const { join } = require('path');
-const { readFileSync } = require('fs');
+const { promises } = require('fs');
 
 const { build } = require('esbuild');
 const { minify } = require('terser');
 const { transformAsync } = require('@babel/core');
+
+const { readFile } = promises;
 
 // file paths
 const ENTRY_FILE_NAME = 'main.js';
 const OUT_FILE_NAME = 'main.bundle.js';
 const entryPath = join(__dirname, `/${ENTRY_FILE_NAME}`);
 const outputPath = join(__dirname, `/${OUT_FILE_NAME}`);
-const jsData = readFileSync(outputPath, 'utf8', data => data);
 
 module.exports = class {
   // template "frontmatter"
@@ -25,14 +26,16 @@ module.exports = class {
 
   async render() {
     try {
-
+      // bundle scripts
       await build({
         entryPoints: [entryPath],
         bundle: true,
         outfile: outputPath,
+        treeShaking: true
       });
+      const data = await readFile(outputPath, { encoding: 'utf8' });
       // transpile with babel; uses local config file and browserslist in package.json
-      const compiled = await transformAsync(jsData).then(result => result.code);
+      const compiled = await transformAsync(data).then(result => result.code);
       const minified = await minify(compiled);
 
       return minified.code;
